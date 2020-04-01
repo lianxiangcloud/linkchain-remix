a328d779fb4b7206e03f9afc0852888e70b3a7ae
haoxu <haoxu@onething.net>
2020-04-01 20:38:58 +0800

    Updates

---
+++ precache-manifest.f01965df215d47fcda1bb55eca5deeca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf03295c6dfaef5935ef5732264d0bc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "39c9680b2325ff207e5f",
+    "url": "./static/css/2.cddfaa18.chunk.css"
+  },
+  {
+    "revision": "1535653d293ce24dc6c1",
+    "url": "./static/css/main.1150bf6d.chunk.css"
+  },
+  {
+    "revision": "39c9680b2325ff207e5f",
+    "url": "./static/js/2.30acfac3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.30acfac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1535653d293ce24dc6c1",
+    "url": "./static/js/main.a34c4f79.chunk.js"
+  },
+  {
+    "revision": "01a8aabf4a1913b6f900",
+    "url": "./static/js/runtime-main.01ef885f.js"
+  }
+]);
